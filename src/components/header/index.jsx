3f121597217c24b99dbc1281f7b25d47d7a08ebc
huunhatdev <nhatnh98@gmail.com>
2023-10-02 07:28:39 +0700

    fix sup

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -3,29 +3,28 @@ import IconAdobe from '../../assets/icons/IconAdobe';
 import IconGithub from '../../assets/icons/IconGithub';
 
 const HeaderPage = () => {
-	return (
-		<div className='header'>
-			<h1 className='header__title'>
-				Representation Engineering: A Top-Down Approach to AI Transparency
-			</h1>
-			<p className='header__member'>
-				Andy Zou1, Zifan Wang2, J. Zico Kolter1,3, Matt Fredrikson1
-			</p>
-			<p className='header__note'>
-				1Carnegie Mellon University, 2Center for AI Safety, 3Bosch Center for AI
-			</p>
-			<div className='header__tool'>
-				<div className='header__tool--item'>
-					<IconAdobe />
-					<span>Paper</span>
-				</div>
-				<div className='header__tool--item'>
-					<IconGithub />
-					<span>Code and Data</span>
-				</div>
-			</div>
-		</div>
-	);
+    return (
+        <div className="header">
+            <h1 className="header__title">Representation Engineering: A Top-Down Approach to AI Transparency</h1>
+            <p className="header__member">
+                Andy Zou<sup>1</sup>, Zifan Wang<sup>2</sup>, J. Zico Kolter<sup>1,3</sup>, Matt Fredrikson<sup>1</sup>
+            </p>
+            <p className="header__note">
+                <sup>1</sup>Carnegie Mellon University, <sup>2</sup>Center for AI Safety, <sup>3</sup>Bosch Center for
+                AI
+            </p>
+            <div className="header__tool">
+                <div className="header__tool--item">
+                    <IconAdobe />
+                    <span>Paper</span>
+                </div>
+                <div className="header__tool--item">
+                    <IconGithub />
+                    <span>Code and Data</span>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default HeaderPage;
